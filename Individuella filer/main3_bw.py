3fb5ef881973ac5a89d199a31652a3712857f281
Björn Winterfjord <bjorn.winterfjord@iths.se>
2024-11-21 11:12:20 +0100

    add main3

--- Individuella filer/main3_bw.py
+++ Individuella filer/main3_bw.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import plotly.graph_objs as go
 import seaborn as sns
 import dash_bootstrap_components as dbc
+import hashlib as hl
 
 # Läs in data
 df = pd.read_csv("../athlete_events.csv")
@@ -52,12 +53,185 @@ def ålders_fördelning_func():
 
 
 
+""" hashar namnen och droppar namn kolumnen """
+hashes = df["Name"].apply(lambda client_num: hl.sha256(client_num.encode()).hexdigest())
+df.insert(1, "SHA Hash Values", hashes)
+drop = df.drop(columns= ["Name"])
+df = drop
 
-# Skapa Dash-app
+""" Tar alla unika sporter """
+sports = df['Sport'].unique()
+
+""" Tabell på antal medaljer per individ i tyskland """
+ger_df = df[df["NOC"] == "GER"]
+medal = ger_df["Medal"].isin(["Gold", "Silver", "Bronze"])
+medals = ger_df[medal]
+color1 = ["silver", "orange", "gold"]
+
+""" Tabell på medaljer som nation i Tyskland """
+temp_df = ger_df.drop_duplicates(subset=["Team","NOC","Games","Year","City","Sport","Event","Medal"])
+ny_team_variabel = temp_df["Medal"].isin(["Gold", "Silver", "Bronze"])
+ny_team_variabel = temp_df[ny_team_variabel]
+
+""" Tabell på medaljer """
+ny_team_variabel["Medaltot"] = 1
+df_grouped = ny_team_variabel.groupby(['Year', 'Medal']).sum().reset_index()
+df_pivot = df_grouped.pivot(index='Year', columns='Medal', values='Medaltot')# Pivot = Year blir x-axeln och Medal blir kolumner med en valör i varje. Values(Medeltot) får summan av varje valör per år.
+df_pivot_g = df_pivot["Gold"]
+df_pivot_s = df_pivot["Silver"]
+df_pivot_b = df_pivot["Bronze"]
+
+#----------------------------------------------------------------------------------------------------------------
+
+def medalj_individ():
+    fig = px.bar(medals, x="Medal", color="Medal", color_discrete_sequence=color1, width=550, height=650)
+    fig.update_layout(title="Tyska Individuella Medaljer", xaxis_title="Valör", yaxis_title="Antal")
+    return dcc.Graph(id="medalj_individ", figure=fig)
+
+def medalj_nation():
+    fig = px.bar(ny_team_variabel, x="Medal", color="Medal", color_discrete_sequence=color1, width=550, height=650)
+    fig.update_layout(title="Tyska Nationella Medaljer", xaxis_title="Valör", yaxis_title="Antal")
+    return dcc.Graph(id="medalj_nation", figure=fig)
+
+#------------------------------------------------------------------------------------------------------------------
+
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-# Definiera app-layout
 app.layout = html.Div([
+
+    dbc.Card(
+        dbc.CardBody([
+            html.H2("Välkommen till Olympiska Spelen!", className="card-title"),
+            html.P("Utforska medaljdata för Tyskland och andra nationer i de Olympiska spelen.", className="card-text"),
+            html.Img(src='/assets/os_ringar.png', style={'width': '15%', 'display': 'block', 'margin': '0 auto'}),
+        ]),
+        className="mb-4 border-primary",  # Border och marginal
+        style={"margin-top": "20px", "textAlign": "center"}
+    ),
+
+    # Rubrik för OLYMPISKA SPELEN
+    html.H1("OLYMPISKA SPELEN", style={"textAlign": "center", "color": "blue"}),
+
+    # Horisontell linje
+    html.Hr(),
+
+    # Rubrik för nästa sektion
+    html.H1("TYSKLAND", style={"textAlign": "center", "color": "black"}),
+
+    # Flexbox container för vänster och höger card
+    dbc.Row([
+        # Stort Card till vänster (8/12 bredd)
+        dbc.Col(
+            dbc.Card(
+                dbc.CardBody([
+                    html.H4("Tyska Medaljer", className="card-title"),
+                    dcc.Tabs([
+                        dcc.Tab(label="Individuella medaljer", children=[medalj_individ()], style={'padding': '20px'}),
+                        dcc.Tab(label="Nationella medaljer", children=[medalj_nation()], style={'padding': '20px'})
+                    ])
+                ]),
+                className="mb-3", style={"max-width": "100%", "margin": "0 auto"}
+            ),
+            width=4  # 8 delar av en 12-kolumn layout för stort card
+        ),
+
+
+        dbc.Col(
+            children=[
+                dbc.Card(
+                    dbc.CardBody([
+                        html.Div(
+                            className="d-flex align-items-center",
+                            children=[
+                                html.Img(src='/assets/guld.png', style={'width': '15%', 'margin-right': '10px'}),  # Bild till vänster
+                                html.Div([
+                                    html.H4("745", className="card-text"),
+                                    html.P("Guld medaljer", className="card-text")
+                                ])
+                            ]
+                        ),
+                    ]),
+                    className="mb-3", style={"height": "150px", "margin": "0"}
+                ),
+
+                dbc.Card(
+                    dbc.CardBody([
+                        html.Div(
+                            className="d-flex align-items-center",
+                            children=[
+                                html.Img(src='/assets/silver.png', style={'width': '15%', 'margin-right': '10px'}),  # Bild till vänster
+                                html.Div([
+                                    html.H4("674", className="card-text"),
+                                    html.P("Silver medaljer", className="card-text")
+                                ])
+                            ]
+                        ),
+                    ]),
+                    className="mb-3", style={"height": "150px", "margin": "0"}
+                ),
+
+                dbc.Card(
+                    dbc.CardBody([
+                        html.Div(
+                            className="d-flex align-items-center",
+                            children=[
+                                html.Img(src='/assets/bronz.png', style={'width': '18%', 'margin-right': '10px'}),  # Bild till vänster
+                                html.Div([
+                                    html.H4("746", className="card-text"),
+                                    html.P("Brons medaljer", className="card-text")
+                                ])
+                            ]
+                        ),
+                    ]),
+                    className="mb-3", style={"height": "150px", "margin": "0"}
+                )
+            ],
+            width=2  # 4 delar av en 12-kolumn layout för den högerkolumnen med små cards
+        ),# Kolumnen stängs här
+
+        dbc.Col(
+        children=[
+            # Card med tabellen
+            dbc.Card(
+                dbc.CardBody([
+                    html.H4("Tysklands OS städer", style={"textAlign": "center"}),
+                    dash_table.DataTable(
+                        columns=[
+                            {"name": "Stad", "id": "stad"},
+                            {"name": "Typ av OS", "id": "os_type"},
+                            {"name": "År", "id": "year"}
+                        ],
+                        data=[
+                            {"stad": "Berlin", "os_type": "Sommar OS", "year": "1916 (Inställt)"},
+                            {"stad": "Berlin", "os_type": "Sommar OS", "year": "1936"},
+                            {"stad": "Garmisch-Partenkirchen", "os_type": "Vinter OS", "year": "1936"},
+                        ],
+                        style_table={'width': '100%', 'margin': '0 auto'},
+                        style_header={'backgroundColor': 'lightblue', 'fontWeight': 'bold'},
+                        style_cell={'textAlign': 'center', 'padding': '10px'},
+                    )
+                ]),
+                className="mb-3", style={"height": "auto", "width": "100%"}  # Flexibelt kort för tabellen
+            ),
+            dbc.Card(
+                dbc.CardBody([
+                    dcc.Dropdown(options=[
+                            {"label": "Gold", "value": "Gold"},
+                            {"label": "Silver", "value": "Silver"},
+                            {"label": "Bronze", "value": "Bronze"}
+                        ],  value= "Gold", id="dropdown-item"
+                    ),
+                    dcc.Graph(id="dd_graph")
+                ])
+            )
+        ]
+        )
+        #skriv en ny kolumn här
+
+
+    ]),# Raden stängs här
+
+
     html.Div([
         html.Div([
             dcc.RadioItems(options=["Deltagarländer", "Medaljländer"], value="Deltagarländer", id='controls-and-radio-item'),
